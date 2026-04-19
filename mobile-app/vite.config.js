4eb39cd4db8e61ee92424c7b84a2ef73456126eb
AshwaniCoder77 <ashwanicoder77@users.noreply.github.com>
2026-04-19 14:42:56 +0530

    initial commit

--- mobile-app/vite.config.js
+++ mobile-app/vite.config.js
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
+=======
+  server: {
+    port: 5173,
+    strictPort: true
+  },
+>>>>>>> 51dbaa4 (Initial commit)
   build: {
     outDir: '../deploy_v5/public'
   }
